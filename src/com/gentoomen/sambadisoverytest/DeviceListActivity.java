1953e760cfe08ef3b4b0143ba0cab3ca8847e7b2
David Fontenot <fontenod@onid.oregonstate.edu>
2012-10-29 17:58:58 -0700

    initial commit

---
+++ src/com/gentoomen/sambadisoverytest/DeviceListActivity.java
@@ -0,0 +1,44 @@
+package com.gentoomen.sambadisoverytest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+
+public class DeviceListActivity extends FragmentActivity
+        implements DeviceListFragment.Callbacks {
+
+    private boolean mTwoPane;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+
+        if (findViewById(R.id.device_detail_container) != null) {
+            mTwoPane = true;
+            ((DeviceListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.device_list))
+                    .setActivateOnItemClick(true);
+        }
+    }
+
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            Bundle arguments = new Bundle();
+            arguments.putString(DeviceDetailFragment.ARG_ITEM_ID, id);
+            DeviceDetailFragment fragment = new DeviceDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.device_detail_container, fragment)
+                    .commit();
+
+        } else {
+            Intent detailIntent = new Intent(this, DeviceDetailActivity.class);
+            detailIntent.putExtra(DeviceDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
